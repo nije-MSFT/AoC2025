660d74471b40901ef44317d23b98edd852e4bdf2
Nick Jennings <nije@microsoft.com>
2025-12-04 00:23:10 -0800

    1

---
+++ 1/Part1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day1
+{
+    internal class Part1
+    {
+        public int ReturnVal;
+
+        public Part1()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+
+            var position = 50;
+
+            foreach (var line in lines)
+            {
+                var distance = int.Parse(line.Substring(1));
+
+                position = line[0] == 'L' ?
+                    (position + 100 - distance) % 100 :
+                    (position + distance) % 100;
+
+                if (position == 0)
+                {
+                    ReturnVal++;
+                }
+            }
+        }
+    }
+}
---
+++ 1/Part2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day1
+{
+    internal class Part2
+    {
+        public int ReturnVal;
+
+        public Part2()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+
+            var position = 50;
+
+            foreach (var line in lines)
+            {
+                var distance = int.Parse(line.Substring(1));
+
+                bool startedAtZero = position == 0;
+
+                position = line[0] == 'L' ?
+                    (position - distance):
+                    (position + distance);
+                 
+                ReturnVal += Math.Abs(position / 100);
+
+                if (!startedAtZero && (position <= 0))
+                {
+                    ReturnVal++;
+                }
+
+                position = ((position % 100) + 100) % 100;
+            }
+        }
+    }
+}
