6aae0d3d29ac864a002643ad2b01de6b0ea68a82
Nick Jennings <nije@microsoft.com>
2025-12-08 00:58:17 -0800

    Day 4 part 1

---
+++ 4/Part1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Day4
+{
+    internal class Part1
+    {
+        public long ReturnVal;
+
+        public List<(int x, int y)> points = new List<(int x, int y)>()
+        {
+            (-1, -1), (0, -1), (1, -1),
+            (-1, 0),           (1, 0),
+            (-1, 1),  (0, 1),  (1, 1)
+        };
+
+        public Part1()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+
+            for (int y = 0; y < lines.Length; y++)
+            {
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    if (lines[y][x] != '@')
+                    {
+                        continue;
+                    }
+
+                    var surrounding = 0;
+
+                    foreach (var point in points)
+                    {
+                        if (GetCharAt(x + point.x, y + point.y, lines) == '@')
+                        {
+                            surrounding++;
+                        }
+                    }
+
+                    if (surrounding < 4)
+                    {
+                        ReturnVal++;
+                    }
+                }
+            }
+        }
+
+        public char GetCharAt(int x, int y, string[] lines)
+        {
+            if (y < 0 || y >= lines.Length)
+            {
+                return ' ';
+            }
+            if (x < 0 || x >= lines[y].Length)
+            {
+                return ' ';
+            }
+            return lines[y][x];
+        }
+    }
+}
---
+++ 4/Part2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4
+{
+    internal class Part2
+    {
+        public long ReturnVal;
+
+        public Part2()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+
+            foreach (var line in lines)
+            {
+                int[] digits = [0,0,0,0,0,0,0,0,0,0,0,0];
+                var position = digits.Length;
+                int x = 0;
+                int foundPos = 0;
+
+                while (position > 0)
+                {
+                    while (x < line.Length)
+                    {
+                        if (x + position > line.Length)
+                        {
+                            break;
+                        }
+
+                        var currentNumber = line[x] - '0';
+
+                        if (currentNumber > digits[Math.Abs(position - 12)])
+                        {
+                            digits[Math.Abs(position - 12)] = currentNumber;
+                            foundPos = x;
+                        }
+                        x++;
+                    }
+
+                    x = foundPos + 1;
+                    position--;
+                }
+
+                long foundNumber = 0;
+
+                for (int i = 0; i < digits.Length ; i++)
+                {
+                    foundNumber = (foundNumber * 10) + digits[i];
+                }
+
+                ReturnVal += foundNumber;
+            }
+        }
+    }
+}
