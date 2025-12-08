feb3f0dd3b6e6cc73afa2acaa36d537614e904cc
Nick Jennings <nije@microsoft.com>
2025-12-08 02:19:00 -0800

    Day 5 part 1

---
+++ 5/Part1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Day5
+{
+    internal class Part1
+    {
+        public long ReturnVal;
+
+        public Part1()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+            var ranges = new List<(long start, long end)>();
+            var ingredients = new List<long>();
+
+            int x = 0;
+
+            while (lines[x] != string.Empty)
+            {
+                var split = lines[x].Split('-');
+                ranges.Add((long.Parse(split[0]), long.Parse(split[1])));
+                x++;
+            }
+
+            x++; // Skip empty line
+            
+            while (x < lines.Length)
+            {
+                ingredients.Add(long.Parse(lines[x]));
+                x++;
+            }
+
+            foreach (var ingredient in ingredients)
+            {
+                foreach (var range in ranges)
+                {
+                    if (ingredient >= range.start && ingredient <= range.end)
+                    {
+                        ReturnVal++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 5/Part2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Day5
+{
+    internal class Part2
+    {
+        public long ReturnVal;
+
+        public Part2()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+            var ranges = new List<(long start, long end)>();
+            var ingredients = new List<long>();
+
+            int x = 0;
+
+            while (lines[x] != string.Empty)
+            {
+                var split = lines[x].Split('-');
+                ranges.Add((long.Parse(split[0]), long.Parse(split[1])));
+                x++;
+            }
+
+            foreach (var range in ranges)
+            {
+            }
+        }
+    }
+}
