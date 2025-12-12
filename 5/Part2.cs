0d5d3f351b89463f92660d63fe76c173c3b225d7
Nick Jennings <nije@microsoft.com>
2025-12-11 22:34:23 -0800

    Day 5 part 2

--- 5/Part2.cs
+++ 5/Part2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using System.Text;
 
 namespace Day5
@@ -13,19 +14,53 @@ namespace Day5
         {
             var lines = File.ReadAllLines("Input.txt");
             var ranges = new List<(long start, long end)>();
-            var ingredients = new List<long>();
 
             int x = 0;
 
             while (lines[x] != string.Empty)
             {
                 var split = lines[x].Split('-');
-                ranges.Add((long.Parse(split[0]), long.Parse(split[1])));
+                var start = long.Parse(split[0]);
+                var end = long.Parse(split[1]);
                 x++;
+
+                int y = 0;
+
+                while (y < ranges.Count)
+                {
+                    if (start >= ranges[y].start && end <= ranges[y].end)
+                    {
+                        break;
+                    }
+                    else if (start >= ranges[y].start && start <= ranges[y].end && end > ranges[y].end)
+                    {
+                        start = Math.Min(start, ranges[y].start);
+                        ranges.RemoveAt(y);
+                    }
+                    else if (end >= ranges[y].start && end <= ranges[y].end && start < ranges[y].start)
+                    {
+                        end = Math.Max(end, ranges[y].end);
+                        ranges.RemoveAt(y);
+                    }
+                    else if (start < ranges[y].start && end > ranges[y].end)
+                    {
+                        ranges.RemoveAt(y);
+                    }
+                    else
+                    {
+                        y++;
+                    }
+                }
+
+                if (y == ranges.Count)
+                {
+                    ranges.Add((start, end));
+                }
             }
 
             foreach (var range in ranges)
             {
+                ReturnVal += (range.end - range.start + 1);
             }
         }
     }
