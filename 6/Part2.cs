5e74a3e8ba2f6a57bff6837920ee2f16cdb53aff
Nick Jennings <nije@microsoft.com>
2025-12-12 00:25:37 -0800

    Day 6 part 2

--- 6/Part2.cs
+++ 6/Part2.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.Metrics;
 using System.Text;
 
 namespace Day6
@@ -13,54 +12,59 @@ namespace Day6
         public Part2()
         {
             var lines = File.ReadAllLines("Input.txt");
-            var ranges = new List<(long start, long end)>();
-
-            int x = 0;
-
-            while (lines[x] != string.Empty)
+            var numberLines = new List<List<string>>
             {
-                var split = lines[x].Split('-');
-                var start = long.Parse(split[0]);
-                var end = long.Parse(split[1]);
-                x++;
+                ([])
+            };
 
-                int y = 0;
+            for (int x = lines[0].Length - 1; x >= 0; x--)
+            {
+                StringBuilder sb = new StringBuilder();
 
-                while (y < ranges.Count)
+                for (int y = 0; y <= lines.Length - 2; y++)
                 {
-                    if (start >= ranges[y].start && end <= ranges[y].end)
-                    {
-                        break;
-                    }
-                    else if (start >= ranges[y].start && start <= ranges[y].end && end > ranges[y].end)
-                    {
-                        start = Math.Min(start, ranges[y].start);
-                        ranges.RemoveAt(y);
-                    }
-                    else if (end >= ranges[y].start && end <= ranges[y].end && start < ranges[y].start)
-                    {
-                        end = Math.Max(end, ranges[y].end);
-                        ranges.RemoveAt(y);
-                    }
-                    else if (start < ranges[y].start && end > ranges[y].end)
-                    {
-                        ranges.RemoveAt(y);
-                    }
-                    else
-                    {
-                        y++;
-                    }
+                    sb.Append(lines[y][x]);
                 }
 
-                if (y == ranges.Count)
+                var number = sb.ToString().Trim();
+
+                if (string.IsNullOrEmpty(number))
                 {
-                    ranges.Add((start, end));
+                    numberLines.Add(new List<string>());
+                }
+                else
+                {
+                    numberLines.Last().Add(number);
+                }
+
+                if (lines[lines.Length - 1][x] !=  ' ')
+                {
+                    numberLines.Last().Add(lines[lines.Length - 1][x].ToString());
                 }
             }
 
-            foreach (var range in ranges)
+            foreach (var line in numberLines)
             {
-                ReturnVal += (range.end - range.start + 1);
+                var operation = line.Last();
+                line.RemoveAt(line.Count - 1);
+
+                long solution = long.Parse(line[0]);
+
+                foreach (var numberAsString in line.Skip(1))
+                {
+                    var number = long.Parse(numberAsString);
+
+                    if (operation == "*")
+                    {
+                        solution *= number;
+                    }
+                    else
+                    {
+                        solution += number;
+                    }
+                }
+
+                ReturnVal += solution;
             }
         }
     }
