08099e29a41ceddc8e2aac04e8ddb5e9d70c3f1f
Nick Jennings <nije@microsoft.com>
2025-12-11 23:06:06 -0800

    Day 6 part 1

---
+++ 6/Part1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Day6
+{
+    internal class Part1
+    {
+        public long ReturnVal;
+
+        public Part1()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+            var numberLines = new List<List<string>>();
+
+            foreach (var line in lines)
+            {
+                int x = 0;
+
+                foreach (var item in line.Split(' '))
+                {
+                    if (item != string.Empty)
+                    {
+                        if (x == numberLines.Count)
+                        {
+                            numberLines.Add(new List<string>());
+                        }
+
+                        numberLines[x].Add(item);
+                        x++;
+                    }
+                }
+            }
+
+            foreach (var line in numberLines)
+            {
+                var operation = line.Last();
+                line.RemoveAt(line.Count - 1);
+
+                long solution = long.Parse(line[0]);
+
+                foreach (var numberAsString in line.Skip(1))
+                {
+                    var number = long.Parse(numberAsString);
+
+                    if (operation == "*")
+                    {
+                        solution *= number;
+                    }
+                    else
+                    {
+                        solution += number;
+                    }
+                }
+
+                ReturnVal += solution;
+            }
+        }
+    }
+}
---
+++ 6/Part2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Text;
+
+namespace Day6
+{
+    internal class Part2
+    {
+        public long ReturnVal;
+
+        public Part2()
+        {
+            var lines = File.ReadAllLines("Input.txt");
+            var ranges = new List<(long start, long end)>();
+
+            int x = 0;
+
+            while (lines[x] != string.Empty)
+            {
+                var split = lines[x].Split('-');
+                var start = long.Parse(split[0]);
+                var end = long.Parse(split[1]);
+                x++;
+
+                int y = 0;
+
+                while (y < ranges.Count)
+                {
+                    if (start >= ranges[y].start && end <= ranges[y].end)
+                    {
+                        break;
+                    }
+                    else if (start >= ranges[y].start && start <= ranges[y].end && end > ranges[y].end)
+                    {
+                        start = Math.Min(start, ranges[y].start);
+                        ranges.RemoveAt(y);
+                    }
+                    else if (end >= ranges[y].start && end <= ranges[y].end && start < ranges[y].start)
+                    {
+                        end = Math.Max(end, ranges[y].end);
+                        ranges.RemoveAt(y);
+                    }
+                    else if (start < ranges[y].start && end > ranges[y].end)
+                    {
+                        ranges.RemoveAt(y);
+                    }
+                    else
+                    {
+                        y++;
+                    }
+                }
+
+                if (y == ranges.Count)
+                {
+                    ranges.Add((start, end));
+                }
+            }
+
+            foreach (var range in ranges)
+            {
+                ReturnVal += (range.end - range.start + 1);
+            }
+        }
+    }
+}
